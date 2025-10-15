ee350ccabac8c94847cc1d3ef2f486844f05a653
Woeter <skippedlead@gmail.com>
2025-10-15 21:47:04 +0530

    Added basic pseudo converter

---
+++ pseudo.py
@@ -0,0 +1,29 @@
+import json
+
+with open("old_english_dictionary.json") as f:
+    old_eng_dict = json.load(f)
+
+
+def convert_to_pseudo(text):
+    words = text.split()
+    result = []
+
+    for word in words:
+        key = word.lower().strip("!?,.")
+
+        converted = old_eng_dict.get(key,word)
+
+        if word[0].isupper():
+            converted = converted.capitalize()
+        
+        result.append(converted)
+
+    return " ".join(result)
+
+
+
+text = "I apologize for my actions"
+
+print(convert_to_pseudo(text))
+
+
